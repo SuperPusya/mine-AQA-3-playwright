aa8d443c151a8718b1097670f0627f9c8f9f4fe2
superpusya <xxx.xxx-xx2018@ya.ru>
2025-04-28 13:22:20 +0400

    сделала task-1 в четвертой домашке по автоматизации

---
+++ src/ui/pages/sign-in.page.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from "@playwright/test";
+import { ModuleName } from "types/home.types";
+import { SalesPortalPage } from "./salesPortal.page";
+import { ILogin } from "types/sign-in.types";
+
+export class SignIn extends SalesPortalPage {
+  emailInput = this.page.locator("#emailinput");
+  passwordInput = this.page.locator("#passwordinput");
+  loginButton = this.page.locator('button[type="submit"]');
+
+  uniqueElement = this.loginButton;
+
+  async fillCredentials(user: Partial<ILogin>) {
+    user.email && (await this.emailInput.fill(user.email));
+    user.password && (await this.passwordInput.fill(user.password));
+  }
+
+  async clickLoginButton() {
+    await this.loginButton.click();
+  }
+}
